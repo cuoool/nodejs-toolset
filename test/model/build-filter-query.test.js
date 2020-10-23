5414a3e48280707dd9a798b1a062b0ca8d064118
Chea Bonnak <chea.bonnak@gmail.com>
2020-10-23 11:18:37 +0700

    Accept callback filter

--- test/model/build-filter-query.test.js
+++ test/model/build-filter-query.test.js
@@ -3,7 +3,7 @@ const { Op } = require('sequelize');
 const buildQueryFilter = require('../../lib/model/build-filter-query');
 
 describe('Query filter', () => {
-  it('Use Op.or operator to wrap around null or empty field', () => {
+  it.only('Use Op.or operator to wrap around null or empty field', () => {
     const result1 = buildQueryFilter({ nullField: null, otherParam: 'text' });
 
     expect(result1).to.deep.equal({
@@ -22,6 +22,28 @@ describe('Query filter', () => {
         { nullField: '' },
       ],
       otherParam: 'text',
+    });
+  });
+
+  it.only('use callback', () => {
+    const filter = { event: 'EventName', fromDate: '2020-01-01', toDate: '2020-10-03' };
+
+    const result = buildQueryFilter(filter, (prop) => ([
+      'event',
+      {
+        createdAt: {
+          [Op.gte]: prop.fromDate,
+          [Op.lte]: prop.fromDate,
+        },
+      },
+    ]));
+
+    expect(result).to.deep.equal({
+      event: 'EventName',
+      createdAt: {
+        [Op.gte]: '2020-01-01',
+        [Op.lte]: '2020-10-03',
+      },
     });
   });
 });
