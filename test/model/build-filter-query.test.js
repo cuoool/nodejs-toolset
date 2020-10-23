cf141bc430e0797def0c4a5ddac6d7a659a421b7
Chea Bonnak <chea.bonnak@gmail.com>
2020-10-23 11:18:57 +0700

    Remove .only

--- test/model/build-filter-query.test.js
+++ test/model/build-filter-query.test.js
@@ -3,7 +3,7 @@ const { Op } = require('sequelize');
 const buildQueryFilter = require('../../lib/model/build-filter-query');
 
 describe('Query filter', () => {
-  it.only('Use Op.or operator to wrap around null or empty field', () => {
+  it('Use Op.or operator to wrap around null or empty field', () => {
     const result1 = buildQueryFilter({ nullField: null, otherParam: 'text' });
 
     expect(result1).to.deep.equal({
